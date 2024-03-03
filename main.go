4e99d5bd8ccf203d7bc71f5278d7d309db175796
Douglas Headley <dheadley@eliassen.com>
2024-03-02 16:09:37 -0800

    can call aircrack from go

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("hello wolrd")
+	// -w
+	// --bsid
+	// pathtofile
+	output, err := exec.Command("/usr/local/bin/aircrack-ng", "-w", "test_passphrases.txt", "wpa_april.cap", "-b", "48:F8:B3:1F:49:F2").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(output))
 }
